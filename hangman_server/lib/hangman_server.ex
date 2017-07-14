bf468097c4053fce7f0cb0a13d1a0dd6e5baa1c1
Dylan Griffith <dyl.griffith@gmail.com>
2017-07-14 19:08:21 +1000

    Initial commit: Generated mix new hangman_server --sup

---
+++ hangman_server/lib/hangman_server.ex
@@ -0,0 +1,18 @@
+defmodule HangmanServer do
+  @moduledoc """
+  Documentation for HangmanServer.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HangmanServer.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ hangman_server/test/hangman_server_test.exs
@@ -0,0 +1,8 @@
+defmodule HangmanServerTest do
+  use ExUnit.Case
+  doctest HangmanServer
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
