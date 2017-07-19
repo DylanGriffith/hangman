89bf1cc087fe69f81c7fce4caf9928b97082ea39
Required For Git Stash When Using Git Duet <stash@example.com>
2017-07-19 18:56:22 +1000

    Add PWS deployment configs

--- hangman_server/lib/hangman_server/application.ex
+++ hangman_server/lib/hangman_server/application.ex
@@ -4,8 +4,10 @@ defmodule HangmanServer.Application do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
+    port = elem(Integer.parse(System.get_env("PORT") || "4001"), 0)
+
     children = [
-      Plug.Adapters.Cowboy.child_spec(:http, HangmanServer.Web.Router, [], [port: 4001]),
+      Plug.Adapters.Cowboy.child_spec(:http, HangmanServer.Web.Router, [], [port: port]),
       supervisor(Registry, [:unique, :sessions_process_registry]),
       supervisor(HangmanServer.Session.Supervisor, []),
       worker(HangmanServer.ScoreKeeper, []),
